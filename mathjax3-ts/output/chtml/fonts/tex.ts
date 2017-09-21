bb10c5112101994133e7f692d7461d1c0bad02ed
Davide P. Cervone <dpvc@union.edu>
2017-09-20 21:55:32 -0400

    Add support for munder/mover/munderover

--- mathjax3-ts/output/chtml/fonts/tex.ts
+++ mathjax3-ts/output/chtml/fonts/tex.ts
@@ -431,7 +431,7 @@ export class TeXFont extends FontData {
         const css: StyleData = {};
         if (options.css) {
             if (options.css & CSS.width)   css.width = this.em(w);
-            if (options.css & CSS.padding) css.padding = this.em(h) + ' 0 ' + this.em(d);
+            if (options.css & CSS.padding) css.padding = this.em(Math.max(0, h)) + ' 0 ' + this.em(Math.max(0, d));
             if (options.css & CSS.content) css.content = '"' + (options.c || this.char(n, true)) + '"';
         }
         if (options.f !== undefined) css['font-family'] = 'MJXZERO, MJXTEX' + (options.f ? '-' + options.f : '');
